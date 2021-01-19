77df8c8dd4fa807c054a440a8314da0dc1ddb4a3
BryanBlancas <orionmunecaycanica@gmail.com>
2021-01-19 14:21:44 -0600

    Implementing reply ticket feature with upload file

--- MyOnlineSupport/UI/src/js/viewModels/view-ticket.js
+++ MyOnlineSupport/UI/src/js/viewModels/view-ticket.js
@@ -9,7 +9,8 @@ define(
     'ojs/ojlistview',
     'ojs/ojarraydataprovider',
     'ojs/ojlistitemlayout',
-    'ojs/ojselectsingle'
+    'ojs/ojselectsingle',
+    'ojs/ojfilepicker'
   ],
   function (oj, ko, $, appUtils, Model, CollectionDataProvider) {
     function ViewTicketViewModel(params) {
@@ -31,6 +32,9 @@ define(
       self.message = ko.observable();
       self.status = ko.observable()
       self.attachment = ko.observable();
+
+      self.uploadedFile = ko.observableArray([]);
+      self.allowedFileTypes = ko.observableArray(['image/*']);
 
 
       /*
@@ -56,44 +60,38 @@ define(
       *
       */
 
-      /*
-      MY TRY
-      var repliesUrl = ko.computed( function(){
-        return "http://localhost:8080/tickets/replies/" + self.ticketId()
-      });
-      var ticketReplyModel = Model.Model.extend({
-        urlRoot: repliesUrl,
-        idAttribute: 'id'
-      });
-      self.replies = new ticketReplyModel();
-      var repliesCollection = new Model.Collection.extend({
-         url: repliesUrl,
-         model: self.replies,
-         comparator: 'id'
-      });
-      self.repliesList = ko.observable(new repliesCollection());
-      self.ticketRepliesDataSource(new CollectionDataProvider(self.repliesList()));
-      self.ticketId.subscribe(function(){
-        self.repliesList.fetch();
-      })*/
-
        /* List View Collection and Model */
       self.ticketRepliesDataSource = ko.observable();
+
+      /* Include the customURL attribute. This is required, so the model knows which end point to call when adding a new item*/
       self.ticketReplyModel = oj.Model.extend({
-        idAttribute: 'id'
+        idAttribute: 'id',
+        customURL: function () {
+          var retObj = {};
+          retObj['url'] = "http://localhost:8080/tickets/replies/" + self.ticketId()
+          return retObj
+        }
       });
+
       var ticketRepliesCollection = oj.Collection.extend({
         customURL: function () {
+          console.log("Inside ticketRepliessCollection: CustomURL");
           var retObj = {};
           retObj['url'] = "http://localhost:8080/tickets/replies/" + self.ticketId()
           return retObj
         },
         model: self.ticketReplyModel
       });
+
       self.ticketReplies = new ticketRepliesCollection();
       self.ticketRepliesDataSource(new oj.CollectionTableDataSource(self.ticketReplies));
+      
       self.ticketId.subscribe(function(){
+        console.log("Inside ticketIdSubscribe");
         self.ticketReplies.fetch();
+        /* Clean the text area and the uploadedFile*/
+        $('#ticket-reply-area').trumbowyg('empty');
+        self.uploadedFile('');
       })
 
 
@@ -121,11 +119,11 @@ define(
         }
 
 
-        /* Function to initialize the trumbowyg (textarea with more functions)*/
-        /*The life cycle event that we need to initialize the editor is going to be handleAttached.
-         This is because all we are waiting on is for the DOM to finish loading and the div that
-         we need to latch on to being present. Therefore, at this point in the life cycle, the element will be present.*/
-       self.handleAttached = function () {
+      /* Function to initialize the trumbowyg (textarea with more functions)*/
+      /*The life cycle event that we need to initialize the editor is going to be handleAttached.
+        This is because all we are waiting on is for the DOM to finish loading and the div that
+        we need to latch on to being present. Therefore, at this point in the life cycle, the element will be present.*/
+      self.handleAttached = function () {
           $('#ticket-reply-area').trumbowyg(
             {
               btns: ['bold', 'italic', 'underline'], // Determine which options are available in the textarea
@@ -135,9 +133,111 @@ define(
           );
         };
 
+      /* Promise to call the file upload function 
+      This means that the function that calls this function will wait for the response*/
+      self.uploadFile = function () {
+        console.log("Inside uploadFile");
+        return new Promise(
+            function (resolve, reject) {
+                var file = $( "#fileUpload" ).find( "input" )[0].files[0];
+                var data = new FormData();
+                data.append("file", file);
+                console.log("Inside promise");
+                $.ajax({
+                    type: "POST",
+                    url: "http://localhost:8080/tickets/upload/" + self.ticketId(),                    
+                    contentType: false,
+                    processData: false,
+                    data: data,
+                    success: function (result) {
+                        resolve("success")
+                        console.log("File uploaded successfully!");
+                    },
+                    error: function (err, status, errorThrown) {
+                        reject(err);
+                        console.error("Error")
+                    }
+                });
+            }
+        )
+      }
+
+      self.fileSelectionListener = function(event){
+        var file = event.detail.files;
+        self.uploadedFile(file);
+      }
+
+      /*Check it an image has been uploaded*/
+      self.ticketReply = function (){
+        var date = new Date();
+        var attachment = [];
+
+        console.log("Inside ticketReply");
+
+        if(self.uploadedFile()[0] != null){
+          console.log("About to upload file");
+          self.uploadFile().then(
+            function(success){
+              attachment = [{
+                "filePath": self.uploadedFile()[0].name,
+                "fileSize": bytesToSize(self.uploadedFile()[0].size),
+                "timestamp": date.toISOString()
+              }]
+              self.addTicketReplyToCollection(attachment, date);
+            })
+            .catch(
+              function(error){
+                console.error("Error uploading file");
+            });
+        }
+        else{
+          console.log("No file selected. Add ticketreply");
+          self.addTicketReplyToCollection(attachment, date);
+        }
+      }
+
+      /* Function to build up the ticket reply and add it to the collection */
+      self.addTicketReplyToCollection = function(attachment, date){
+        console.log("Inside addTicketReplyToCollection");
+        var newReply = {
+            "author": "Charlotte Illidge",
+            "timestamp": date.toISOString(),
+            "note": $('#ticket-reply-area').trumbowyg('html'),
+            "attachment": attachment
+        };
+        console.log("Inside addTicketReplyToCollection: before create");
+        console.log(newReply);
+        self.ticketReplies.create(newReply, {
+            wait: true,
+            success: function(model, response, options){
+                console.log("Success to create ticketReply");
+            },
+            error: function(err, status, errorThrown){
+                console.error("Error");
+            }
+        });
+        console.log("Inside addTicketReplyToCollection: after create");
+        $('#ticket-reply-area').trumbowyg('empty');
+        self.uploadedFile('');
+      }
+
 
       /* Utils */
+      // Function to format the date
       self.formatDate = appUtils.formatDate;   
+
+      // Function to convert bytes to size
+      function bytesToSize(bytes) {
+        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        if (bytes == 0) return 'n/a';
+          var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+          return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+      };
+
+      /* Function to automatically scroll the user to the reply editor */
+      self.scrollToReply = function(){
+        document.getElementById('ticket-reply-area').scrollIntoView();
+      }
 
     }
   return ViewTicketViewModel;
