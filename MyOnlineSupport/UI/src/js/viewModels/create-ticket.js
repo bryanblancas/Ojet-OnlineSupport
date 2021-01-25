43f66d91d81f297ac6e3bca61f7f44bbcee82626
BryanBlancas <orionmunecaycanica@gmail.com>
2021-01-25 11:40:48 -0600

    Adding Form Skeleton for create ricket

--- MyOnlineSupport/UI/src/js/viewModels/create-ticket.js
+++ MyOnlineSupport/UI/src/js/viewModels/create-ticket.js
@@ -1,14 +1,31 @@
 define(['ojs/ojcore',
 	'knockout',
-    'jquery'
+    'jquery',
+    'ojs/ojselectcombobox',
+    'trumbowyg'
 ], function (oj, ko, $) {
     function CreateTicketViewModel (params) {
 	    var self = this;
 
+	    /*
+	    *
+	    * VARIABLES
+	    *
+	    */
+	    self.newTicketTitle = ko.observable();
+		self.newTicketPriority = ko.observable();
 
 	    // Animation of the create ticket div
 	    self.handleAttached = function () {
       		oj.AnimationUtils['slideIn']($('#create-new-ticket')[0], { 'direction': 'bottom' });
+      		// Initializing trumbowyg
+			$('#new-ticket-area').trumbowyg(
+	            {
+	                btns: ['bold', 'italic', 'underline'],
+	                resetCss: true,
+	                removeformatPasted: true
+	            }
+			);
 		}
 
     }
