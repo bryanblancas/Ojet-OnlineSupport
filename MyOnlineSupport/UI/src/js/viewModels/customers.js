574c8fe3f5de30d3a091eee6b6c36c05c48bcefd
BryanBlancas <orionmunecaycanica@gmail.com>
2021-02-12 14:47:38 -0600

    Add createAndDownloadFile function, a properly function to create and download a file given the data, name and type of file

--- MyOnlineSupport/UI/src/js/viewModels/customers.js
+++ MyOnlineSupport/UI/src/js/viewModels/customers.js
@@ -8,22 +8,55 @@
 /*
  * Your customer ViewModel code goes here
  */
-define(['accUtils'],
- function(accUtils) {
+define(['ojs/ojcore',
+        'accUtils'],
+ function(oj, accUtils) {
     function CustomerViewModel() {
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
 
       var information = null;
 
-      // Triying to create file
-      self.createFile = function(data){
-        console.log(data);
-      }
-
       self.onButtonPressed = function(){
         information = document.getElementById("information").textContent;
-        createFile(information);
+        let test = `nombre,atributo 1,atributo 2,atributo 3
+        Bryan,HOLA,ASDF,asdf
+        Israel,asdf,asdf,asdf
+        Blancas,5361,566,xxx`;
+        createAndDownloadFile(information, "prueba.csv", "text/csv");
+      }
+
+
+      self.createAndDownloadFile = function(data, name, type){
+      
+        /******************************** 
+         * Create the file object (blob)
+        *********************************/
+        // https://developer.mozilla.org/es/docs/Web/API/Blob/Blob
+        // https://www.iana.org/assignments/media-types/media-types.xhtml#text
+        let file = new Blob([data], {type: type});
+
+        /******************************** 
+         * Create the object URL
+        *********************************/
+        // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+        let url = URL.createObjectURL(file);
+
+        /******************************** 
+         * Create dummy element a to download the file
+         * with the attribute 'download'
+        *********************************/
+        let a = document.createElement("a");
+        // Assign the URL object to the new tag 'a' just created
+        a.href = url;
+        // Download attribute specifies that the target 
+        // will be downloaded when a user clicks on the hyperlink
+        a.download = name;
+        // Manually 'click' the hyperlink
+        a.click();
+
+        // Release the url object
+        URL.revokeObjectURL(url);
       }
 
       /**
